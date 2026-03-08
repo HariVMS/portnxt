ea21ae34078b9fa3bbfac9959de1e8ea1f5d233f
Hariprathap-converse <hariprathap@conversedatasolutions.com>
2026-03-08 18:56:50 +0530

    homepage image update

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -59,9 +59,9 @@ export default function Hero() {
             <div className="relative w-full  animate-float">
               {/* 3D Image */}
               <Image
-                src="/img/Home/hero-img.webp"
+                src="/service-hero/Portnxt/Home/home-page.png"
                 alt="Portnext Innovation Digital Solutions"
-                width={700}
+                width={600}
                 height={700}
                 className=" object-contain drop-shadow-2xl"
                 priority
